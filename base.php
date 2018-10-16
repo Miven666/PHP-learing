7ceddde1dc44cb58c0143e187355671a9d1b2355
mingzhi.xie <mingzhi.xie@yintech.cn>
2018-10-16 09:42:08 +0800

    If...Else 语句、Switch 语句、数组、数组排序

--- base.php
+++ base.php
@@ -157,8 +157,8 @@ $g = "100";
 var_dump($f != $g);     // false
 var_dump($f !== $g);    // true
 // 逻辑运算符
-var_dump(true && false);
-var_dump(true and false);   // and: 都为true，返回true, 我暂时理解为&&
+var_dump(true && false);    // 比and优先级高
+var_dump(true and false);   // and: 都为true，返回true
 var_dump(true xor true); // xor: 有且仅有一个为true，返回true
 // 数组运算符
 $h = array("a" => "red", "b" => "green");
@@ -172,9 +172,106 @@ var_dump($h === $i);    //相同的键值对，且顺序、类型相同
 var_dump(true ? true : false);
 // PHP 5.3+ 版本写法
 var_dump(true ?: false);
+// 组合比较符
+echo 1 <=> 2; // -1
+echo "<br>";
+echo 1.2 <=> 2.4; // -1
+echo "<br>";
+echo "a" <=> "b"; // -1
+echo "<br>";
+echo "e" <=> "a"; // 1
+echo "<br>";
+
+/**
+ * If...Else 语句
+ */
+// 如果当前时间小于 20，下面的实例将输出 "Have a good day!"
+$t = date("H");
+if ($t < 20) {
+    echo "Have a good day!";
+    echo "<br>";
+}
+
+/**
+ * Switch 语句
+ */
+$color = "red";
+switch ($color) {
+    case "red":
+        echo "我喜欢的是红色";
+        echo "<br>";
+        break;
+    case "blue":
+        echo "我喜欢的蓝色";
+        break;
+    default:
+        echo "默认颜色";
+}
+
+/**
+ * 数组
+ */
+// 数值数组，自动分配 ID 键（ID 键总是从 0 开始）
+$list = array("list1", 2, 3.0);
+echo "list[0] = " . $list[0] . ", " . "list[1] = " . $list[1] . ", " . "list[2] = " . $list[2] . ", 长度是：" . count($list);   // 获取数组的长度: count() 函数
+echo "<br>";
+// 遍历数值数组
+for ($i = 0; $i < count($list); $i++) {
+    echo $list[$i];
+    echo "<br>";
+}
+// 关联数组
+$map = array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+echo "Map is " . $map["Joe"] . " years old!";
+echo "<br>";
+// 遍历关联数组
+foreach ($map as $key => $value) {
+    echo "Key = " . $key . ", Value = " . $value;
+    echo "<br>";
+}
 
 
+/**
+ * 数组排序
+ */
+// sort() - 对数组进行升序排列
+$sort = array("sort3", "sort2", "sort1");
+var_dump($sort);
+echo "<br>";
+sort($sort);
+var_dump($sort);
 
+// rsort() - 对数组进行降序排列
+$rsort = array("rsort1", "rsort2", "rsort3");
+var_dump($rsort);
+echo "<br>";
+rsort($rsort);
+var_dump($rsort);
 
+// asort() - 根据数组的值，对数组进行升序排列
+$asort = array("asort1" => 3, "asort2" => 2, "asort3" => 1);
+var_dump($asort);
+echo "<br>";
+asort($asort);
+var_dump($asort);
 
+// ksort() - 根据数组的键，对数组进行升序排列
+$ksort = array("ksort3" => 1, "ksort2" => 2, "ksort1" => 3);
+var_dump($ksort);
+echo "<br>";
+ksort($ksort);
+var_dump($ksort);
 
+// arsort() - 根据数组的值，对数组进行降序排列
+$arsort = array("arsort1" => 1, "arsort2" => 2, "arsort3" => 3);
+var_dump($arsort);
+echo "<br>";
+arsort($arsort);
+var_dump($arsort);
+
+// krsort() - 根据数组的键，对数组进行降序排列
+$krsort = array("krsort1" => 1, "krsort2" => 2, "krsort3" => 3);
+var_dump($krsort);
+echo "<br>";
+krsort($krsort);
+var_dump($krsort);
